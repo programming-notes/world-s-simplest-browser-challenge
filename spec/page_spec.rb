b088ae96c6e35d6401c5c4dd4e351807180b9804
Torey Hickman <torey@toreyhickman.com>
2015-11-11 15:02:23 -0600

    Modify Page
    - Remove fetch! from the class.  The class represents a page, it
      would seem to need to be responsible for making HTTP requests.

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -1,6 +1,21 @@
 require_relative '../page'
 
-describe "Page" do
+describe Page do
+  let(:page) { Page.new(url: 'http://somedomain.com', title: 'Site Name', character_count: 500, links: ['http://anotherdomain.com', 'http://somedomain.com/register']) }
 
-  pending "write some specs for your page object"
+  it 'has a url' do
+    expect(page.url).to eq 'http://somedomain.com'
+  end
+
+  it 'has a title' do
+    expect(page.title).to eq 'Site Name'
+  end
+
+  it 'has a character count' do
+    expect(page.character_count).to eq 500
+  end
+
+  it 'has a collection of links' do
+    expect(page.links).to match_array ['http://anotherdomain.com', 'http://somedomain.com/register']
+  end
 end
